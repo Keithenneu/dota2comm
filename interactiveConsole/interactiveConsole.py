67300774988357dde94d4d26682e91b2998851a9
lenLRX <545976176@qq.com>
2017-02-03 00:02:11 +0800

    1:multiple bots support 2:mem leak fixes

--- interactiveConsole/interactiveConsole.py
+++ interactiveConsole/interactiveConsole.py
@@ -4,34 +4,44 @@ from time import sleep
 
 from dota2comm import Dota2Comm
 
-if len(sys.argv) == 2:
-    dota = Dota2Comm(path=sys.argv[1])
-else:
-    dota = Dota2Comm()
+class InteractiveConsole():
+    def __init__(self, name):
+        self.name = name
+        self.dota = Dota2Comm(self.name)
+    
+    def StartThreads(self):
+        self.running = True
+        self.threadrecv = Thread(target=self.waitForAnswers)
+        self.threadrecv.start()
+        self.threadsend = Thread(target=self.sendRequests)
+        self.threadsend.start()
+    
+    def waitForAnswers(self):
+        while self.running:
+            msg = self.dota.receiveMessage()
+            if msg is not None:
+                print("%s: [Out] %s" % (self.name,str(msg)))
+            sleep(0.05)
+    
+    def sendRequests(self):
+        while self.running:
+            try:
+                print("%s: [In] " % (self.name), end="")
+                msg = input()
+                if msg == "exit":
+                    self.dota.exit()
+                    self.running = False
+                    self.threadrecv.join()
+                else:
+                    self.dota.sendMessage(msg)
+                sleep(0.1)
+            except:
+                self.running = False
+                sys.exit()
 
-def waitForAnswers():
-    while running:
-        msg = dota.receiveMessage()
-        if msg is not None:
-            print("[Out] %s" % str(msg))
-        sleep(0.05)
-    
-running = True
-    
-thread = Thread(target=waitForAnswers)
-thread.start()
-    
-while running:
-    try:
-        print("[In] ", end="")
-        msg = input()
-        if msg == "exit":
-            dota.exit()
-            running = False
-            thread.join()
-        else:
-            dota.sendMessage(msg)
-        sleep(0.1)
-    except:
-        running = False
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./interactiveConsole.py marker")
         sys.exit()
+    console = InteractiveConsole(sys.argv[1])
+    console.StartThreads()

c796aca55b8ed3a206ebc800167c6c4da6091d9a
Keithen <Keithen.Neu@gmail.com>
2017-02-02 19:54:39 +0100

    modify filecomm to work with multiple bots

--- dota2FileComm/dota2comm.lua
+++ dota2FileComm/dota2comm.lua
@@ -1,37 +1,51 @@
-module("comm", package.seeall)
+local comm = {}
 
-setfenv(1, comm);
+function comm:new(marker)
+	print("comm init " .. marker)
 
-currentrev = 0
-queue = {}
+    local o = {}
+	setmetatable(o, self)
+	self.__index = self
 
-function comm.send(text)
-    print("dota2comm: "..text)
+    o.marker = marker
+    o.currentrev = 0
+    o.queue = {}
+    return o
 end
 
-function comm.receive()
-    result, err = loadfile(GetScriptDirectory().."/dota2comm/rev")
+function comm:send(text)
+    print("dota2comm "..self.marker..": "..text)
+end
+
+function comm:receive()
+    local result, err = loadfile(GetScriptDirectory().."/dota2comm/rev_"..self.marker)
     if (err ~= nil) then
         return nil
     end
-    result(); -- should set rev
-    if (rev == nil) then rev = 0 end
-    if (rev > currentrev) then
-        currentrev = rev
-        result, err = loadfile(GetScriptDirectory().."/dota2comm/messages")
-        if (err ~= nil) then 
+    local env = {}
+    setfenv(result, env)
+    result() -- should set env.rev
+    if (env.rev == nil) then env.rev = 0 end
+    if (env.rev > self.currentrev) then
+        self.currentrev = env.rev
+        result, err = loadfile(GetScriptDirectory().."/dota2comm/messages_"..self.marker)
+        if (err ~= nil) then
             print("commerror: "..err)
             return nil
         end
-        result(); -- should set messages
-        if (messages == nil) then messages = {} end
-        for i = 1,#messages do
-            if (messages[i] ~= "") then
-                queue[#queue + 1] = messages[i]
+        local env = {}
+        setfenv(result, env)
+        result() -- should set env.messages
+        if (env.messages == nil) then env.messages = {} end
+        for i = 1,#env.messages do
+            if (env.messages[i] ~= "") then
+                self.queue[#self.queue + 1] = env.messages[i]
             end
         end
     end
-    if (#queue > 0) then
-        return table.remove(queue, 1)
+    if (#self.queue > 0) then
+        return table.remove(self.queue, 1)
     end
 end
+
+return comm
--- dota2FileComm/dota2comm.py
+++ dota2FileComm/dota2comm.py
@@ -1,29 +1,36 @@
 import os
 
 class Dota2Comm:
-    def __init__(self, path=None, **kwargs):
-        if path is None:
-            raise ValueError("No path to dota2 bot directory provided")
-            
-        self.__path = path
-            
-        if not os.path.exists(path + "/dota2comm/"):
-            os.makedirs(path + "/dota2comm")
-            
-        if os.path.exists(path + "/dota2comm/rev.lua"):
-            self.__rev = int(open(path + "/dota2comm/rev.lua", "r").read().split("=")[1])
+    def __init__(self, name):
+        if os.path.exists("path.txt"):
+            self.__path = open("path.txt", "r").read()
+        else:
+            self.__path = input("Enter the Dota2 bot script directory: ")
+            f = open("path.txt", "w")
+            f.write(self.__path)
+            f.close()
+
+        print("Using path " + self.__path)
+
+        self.__name = name
+
+        if not os.path.exists(self.__path + "/dota2comm/"):
+            os.makedirs(self.__path + "/dota2comm")
+
+        if os.path.exists(self.__path + "/dota2comm/rev_%s.lua" % name):
+            self.__rev = int(open(self.__path + "/dota2comm/rev_%s.lua" % name, "r").read().split("=")[1])
         else:
             self.__rev = 0
-            
+
     def __send(self, msg):
         self.__rev = self.__rev + 1
         messages = "messages = {\"%s\"}" % msg.replace("\\", "\\\\").replace("\"", "\\\"")
 
-        f = open(self.__path + "/dota2comm/messages.lua", "w")
+        f = open(self.__path + "/dota2comm/messages_%s.lua" % self.__name, "w")
         f.write(messages)
         f.close()
 
-        f = open(self.__path + "/dota2comm/rev.lua", "w")
+        f = open(self.__path + "/dota2comm/rev_%s.lua" % self.__name, "w")
         f.write("rev=" + str(self.__rev))
         f.close()
 
@@ -33,6 +40,6 @@ class Dota2Comm:
     def sendMessage(self, message):
         self.__send(message)
         return True
-        
+
     def exit(self):
         self.__send("")
